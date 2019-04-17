389e2aba367b7b29fbe56ae2ff58a9d1a6c973a0
DiegoMatheus <GM@Diegos-mbp.local>
2019-04-16 21:52:01 -0400

    minor changes

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ module.exports = {
   URL: process.env.BASE_URL || "http:localhost:8000",
   MONGODB_URI:
     process.env.MONGODB_URI ||
-    "mongodb://diegomfg:diego1210@ds147684.mlab.com:47684/report_system",
+    "URL_DATABASE_GOES_HERE",
   SECRET_KEY: process.env.JWT_SECRET || "secretkey"
 };
