8e51db775d9da30d58f7d17189d9b7962453a57c
DiegoMatheus <GM@Diegos-mbp.local>
2019-04-23 18:15:55 -0400

    Deleted client service

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ module.exports = {
   URL: process.env.BASE_URL || "http:localhost:8000",
   MONGODB_URI:
     process.env.MONGODB_URI ||
-    "URL_DATABASE_GOES_HERE",
+    "mongodb://diegomfg:diego1210@ds147684.mlab.com:47684/report_system",
   SECRET_KEY: process.env.JWT_SECRET || "secretkey"
 };
