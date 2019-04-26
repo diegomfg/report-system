dce8c8ee7949a90ea5b2b9e49b85b195fa4bd958
DIEGO-PC\diego <diegoamatheus@gmail.com>
2019-04-25 22:25:32 -0400

    added config.js file, don't know how i removed it

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    ENV: process.env.NODE_ENV || "development",
+    PORT: process.env.PORT || 8000,
+    URL: process.env.BASE_URL || "http:localhost:8000",
+    MONGODB_URI:
+      process.env.MONGODB_URI ||
+      "mongodb://diegomfg:diego1210@ds147684.mlab.com:47684/report_system",
+    SECRET_KEY: process.env.JWT_SECRET || "secretkey"
+  };
