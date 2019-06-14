73e1792118970e3dd291cb96f76fa96bad08736f
diego <diegoamatheus@gmail.com>
2019-06-14 18:34:29 -0400

    Added a dummy .env file

--- public/scripts/edit.js
+++ public/scripts/edit.js
@@ -1,9 +1,9 @@
 // $("#edit-form").on('submit', (event) => {
-//   event.preventDefault();
-//   const report = {
-//     title: $('#title').val(),
-//     body: $('#body').val(),
-//     area: $('#area').val()
-//   }
-//   fetch('/log/edit/')
+// event.preventDefault();
+// const report = {
+// title: $('#title').val(),
+// body: $('#body').val(),
+// area: $('#area').val()
+// }
+// fetch('/log/edit/<%-report.id%>')
 // })
